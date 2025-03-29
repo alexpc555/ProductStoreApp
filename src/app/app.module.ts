44a368e7c16cd679f5fc5c6c55fc3d86fe53ff68
alexpc555 <67518097+alexpc555@users.noreply.github.com>
2025-03-28 19:12:42 -0600

    modificacion
    modificacion de botones y models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,11 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
 
 
 
@@ -33,7 +38,9 @@ import {MatExpansionModule} from '@angular/material/expansion';
     MatButtonModule,
     MatCardModule,
     MatIconModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatTooltipModule,
+    BrowserAnimationsModule
 
   ],
   providers: [],
